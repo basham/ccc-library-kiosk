59a10bfb863b46f073186448144f26c71676900b
Chris Basham <chris@bash.am>
2021-08-28 15:18:32 -0400

    Transition to new logos and links

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ECC Library Kiosk
+# CCC Library Kiosk
 
-Chrome extension for the library kiosk at [Evangelical Community Church](http://www.eccbloomington.org/). This extension reskins the [LibraryWorld Self Service](http://circ.libraryworld.com/selfservice.html) app.
+Chrome extension for the library kiosk at [Chris Community Church](https://cccbloomington.org). This extension is the frontpage for the admin site, public catalog, and self-checkout software, powered by [Surpass Cloud](https://surpasssoftware.com).
 
 ## Install
 
@@ -11,7 +11,7 @@ Chrome extension for the library kiosk at [Evangelical Community Church](http://
 5. Enable `Developer mode`.
 6. Click `Load unpacked` button.
 7. Select the source code folder.
-8. In the new **ECC Library** extension card, click the `Details` button.
+8. In the new **CCC Library** extension card, click the `Details` button.
 9. Click the `Extensions options` button.
 10. Enter Library World credentials and update other options as desired.
 11. Delete the source code folder and ZIP file.
--- content-banner.js
+++ content-banner.js
@@ -6,37 +6,47 @@ chrome.storage.local.get('externalBanner', ({ externalBanner }) => {
 
 function renderBanner () {
   const banner = document.createElement('div')
-  banner.classList.add('ecclib-banner')
+  banner.classList.add('ccclib-banner')
   banner.setAttribute('role', 'banner')
   banner.innerHTML = `
     <style>
-      .ecclib-banner {
-        --color-light-gray: #cccccc;
+      .ccclib-banner {
         --color-black: #333333;
-        --color-orange: #b06110;
         --color-white: #ffffff;
-        background-color: #ffffff;
-        border-bottom: calc(1em/16) solid var(--color-light-gray);
-        color: var(--color-black);
+        align-items: center;
+        background-color: var(--color-black);
+        border-bottom: calc(1em/16) solid var(--color-white);
+        color: var(--color-white);
+        display: flex;
         font-family: system-ui, sans-serif;
         font-size: 16px;
         line-height: 1.5rem;
-        padding: 1em;
+        padding: 0.5em;
       }
-      .ecclib-banner a {
-        color: var(--color-orange);
+      .ccclib-banner img {
+        margin-right: 0.5em;
+      }
+      .ccclib-banner a {
+        color: var(--color-white);
+        cursor: pointer !important;
         font-weight: normal;
-        text-decoration: underline;
+        text-decoration: underline !important;
       }
-      .ecclib-banner a:hover {
-        color: var(--color-orange);
+      .ccclib-banner a:hover {
+        color: var(--color-white);
       }
-      .ecclib-banner a:focus {
-        outline: calc(2em/16) solid var(--color-orange);
+      .ccclib-banner a:focus {
+        outline: calc(2em/16) solid var(--color-white) !important;
         outline-offset: calc(2em/16);
       }
+      /* Style adjustments for self-checkout software. */
+      app-root .main-height {
+        --ccclib-banner-height: 49px;
+        height: calc(100vh - 258px - var(--ccclib-banner-height));
+      }
     </style>
-    Back to <a href="${chrome.runtime.getURL('blank.html')}">Evangelical Community Church Library</a>
+    <img alt="" height="32" width="32" src="${chrome.runtime.getURL('icon128.png')}">
+    <span>Return to <a href="${chrome.runtime.getURL('blank.html')}">Christ Community Church Library</a></span>
   `
   document.body.prepend(banner)
 }
